8fe37228498e174ce8e3b28e3400c0a55380ff04
Julliana Sousa <jullianasantossousa@hotmail.com>
2024-07-26 00:00:56 +0100

    test again

--- src/routes/story/create/community-stories/+page.server.ts
+++ src/routes/story/create/community-stories/+page.server.ts
@@ -349,7 +349,7 @@ export const actions = {
 				const uploadStream = cloudinary.uploader.upload_stream(
 					{
 						resource_type: 'video',
-						eager: [{ format: 'wav' }],
+						eager: [{ format: 'mpeg' }],
 						eager_async: true,
 					},
 					(error, result) => {
@@ -372,7 +372,7 @@ export const actions = {
 
 			// Create a File-like object
 			const audioBuffer = response.data;
-			const audioFile = new File([audioBuffer], 'audio_file.wav', { type: 'audio/wav' });
+			const audioFile = new File([audioBuffer], 'audio_file.mpeg', { type: 'audio/mpeg' });
 
 			console.log('Audio File:', audioFile);
 			return audioFile;
